7eb01088594dabfad6834372a44a911ae78072f8
PhAr4ujo <phcampos0401@gmail.com>
2026-05-26 11:39:57 -0300

    remove(seeder): remove default seeder test.

--- tests/Feature/DatabaseSeederTest.php
+++ tests/Feature/DatabaseSeederTest.php
@@ -9,16 +9,4 @@ use Tests\TestCase;
 class DatabaseSeederTest extends TestCase
 {
     use RefreshDatabase;
-
-    public function test_database_seeder_can_run_more_than_once(): void
-    {
-        $this->seed(DatabaseSeeder::class);
-        $this->seed(DatabaseSeeder::class);
-
-        $this->assertDatabaseCount('users', 1);
-        $this->assertDatabaseHas('users', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
 }
